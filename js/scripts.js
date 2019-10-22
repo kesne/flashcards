466e760f83c81fbf77b60b1dd5a27860d225d530
Student <example@example.com>
2019-10-22 13:30:54 -0700

    enter input information, write javascript to make site responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
-
+  $(".flashcard").click(function() {
+    $(".flashcard-initial", this).fadeToggle();
+    $(".flashcard-reveal", this).fadeToggle();
+  });
 
 });
+
+
+// This is the difficult way to do it (these lines for every single card)
+
+// $("#flashcard-1").click(function() {
+//   $(".flashcard1-initial").fadeToggle();
+//   $(".flashcard1-reveal".fadeToggle();
+// });
